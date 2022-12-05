ac914e91a534914d519f1a7fc9291838bf27cfc1
Aigerim <abdulakimovaaigerim205@gmail.com>
2022-12-05 11:33:11 +0600

    commit2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Random random = new Random();
+        int[] array = new int[10];
+        for (int i = 0; i <array.length ; i++) {
+            array[i]=random.nextInt(0,2);
+        }
+        ArrayList<Integer> ArrayNumbers = new ArrayList<>();
+        for (int i = 0; i < 10; i++)
+            ArrayNumbers.add(random.nextInt(0, 2));
+
+        LinkedList<Integer> LinkedNumbers = new LinkedList<>();;
+        for (int i = 0; i < 10 ; i++)
+            LinkedNumbers.add(random.nextInt(0,2));
+
+
+        sortArrays(array);
+        sortArrays(ArrayNumbers);
+        sortArrays(LinkedNumbers);
+
+    }
+    public static void sortArrays(int[]numbers){
+        Arrays.sort(numbers);
+        System.out.println("Array: "+Arrays.toString(numbers));
+
+    }
+    public static void sortArrays(ArrayList<Integer>numbers){
+        Collections.sort(numbers);
+        System.out.println("ArrayList: "+numbers);
+    }
+    public static void sortArrays(LinkedList<Integer>numbers){
+        Collections.sort(numbers);
+        System.out.println("LinkedList: "+numbers);
     }
 }
